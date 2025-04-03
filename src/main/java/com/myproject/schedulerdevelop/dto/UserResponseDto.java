5a26a870ccfd42497acc049870dfaf67d799b0da
Jess <jcscene@gmail.com>
2025-04-04 02:37:53 +0900

    feat: 리스폰스디티오 구현

--- src/main/java/com/myproject/schedulerdevelop/dto/UserResponseDto.java
+++ src/main/java/com/myproject/schedulerdevelop/dto/UserResponseDto.java
@@ -1,4 +1,11 @@
 package com.myproject.schedulerdevelop.dto;
 
+import lombok.Getter;
+
+@Getter
 public class UserResponseDto {
+    private Long id;
+    private String userName;
+    private String email;
+
 }
