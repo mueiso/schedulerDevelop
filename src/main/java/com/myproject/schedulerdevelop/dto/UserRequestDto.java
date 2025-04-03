1b32febf076a181fab4c6b68c22cc7dfe2109e6e
Jess <jcscene@gmail.com>
2025-04-04 02:13:45 +0900

    feat: Dto 구현

--- src/main/java/com/myproject/schedulerdevelop/dto/UserRequestDto.java
+++ src/main/java/com/myproject/schedulerdevelop/dto/UserRequestDto.java
@@ -1,4 +1,16 @@
 package com.myproject.schedulerdevelop.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
 public class UserRequestDto {
+
+    @NotNull(message = "유저명은 필수로 입력해야 합니다.")
+    private String userName;
+
+    private String email;
+
+    @NotNull(message = "비밀번호는 필수로 입력해야 합니다.")
+    private String password;
 }
